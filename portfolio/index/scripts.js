fdaac8ff65127f7b9977baa202d07e8dd685fa89
vittorioPiotti <vittoriopiotti.vp@gmail.com>
2024-06-18 00:46:30 +0200

    Add files via upload

---
+++ portfolio/index/scripts.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('#About-Me').click(function() {
+            var $this = $(this);
+            $this.addClass('active');
+            
+            // Rimuovi la classe active dopo 0.4 secondi
+            setTimeout(function() {
+                $this.removeClass('active');
+            }, 500);
+        });
+    });
+
+
+
+    $(document).ready(function() {
+        // Funzione per aggiornare il padding degli elementi nav-elem
+        function updateNavElemPadding() {
+            if ($('.navbar-toggler').attr('aria-expanded') === 'true') {
+                $('.nav-elem').css({
+                    'padding-top': '0.5rem',
+                    'padding-bottom': '0.5rem',
+                    'padding-left': '0',
+                    'padding-right': '0'
+                });
+            } else {
+                $('.nav-elem').css({
+                    'padding': '0.5rem 1rem'
+                });
+            }
+        }
+    
+        // Aggiorna il padding quando la navbar viene aperta o chiusa
+        $('.navbar-toggler').on('click', function() {
+            // Determina il timeout in base allo stato di aria-expanded
+            var isOpen = $('.navbar-toggler').attr('aria-expanded') === 'true';
+            var timeout = isOpen ? 250 : 0;
+    
+            setTimeout(updateNavElemPadding, timeout); // Timeout per aspettare la fine dell'animazione del toggle
+        });
+    
+        // Aggiorna il padding al caricamento della pagina
+        updateNavElemPadding();
+    });
+    
+    
+    $(document).ready(function() {
+       // Seleziona tutti gli elementi della nav con la classe "nav-elem"
+    $('.nav-elem').on('click', function() {
+        var navbarToggler = $('.navbar-toggler');
+        if (navbarToggler.attr('aria-expanded') === 'true') {
+            setTimeout(function() {
+                navbarToggler.click();
+            }, 500); // Timeout di 200ms prima di chiudere la navbar
+        }
+    });
+    });
+    $(document).ready(function() {
+        $('.navbar-nav .nav-elem').click(function() {
+            $('.navbar-nav .nav-elem').removeClass('active');
+            var $this = $(this);
+            $this.addClass('active');
+            
+            // Rimuovi la classe active dopo 0.4 secondi
+            setTimeout(function() {
+                $this.removeClass('active');
+            }, 500);
+        });
+    });
+    
+    function adjustLayout() {
+        if ($(window).width() <= 766) {
+            $('#left-content').css({
+                'flex-direction': 'row',
+                'align-items': 'center',
+                'justify-content': 'flex-start',
+                'text-align': 'center'
+            });
+            $('#responsive-image').css({
+                'width': '90px',
+                'height': '90px'
+            });
+            $('#inner-left-content').addClass('text-start').removeClass('text-center').addClass('d-flex').removeClass('d-block');
+            $('#status-space').addClass('m-0');
+            $('p').css({
+                'text-align': 'left',
+                'margin-left': '1rem'
+            });
+        } else {
+            $('#left-content').css({
+                'text-align': 'center'
+            });
+            $('#responsive-image').css({
+                'width': '290px',
+                'height': '290px'
+            });
+            $('#inner-left-content').removeClass('text-start').addClass('text-center').removeClass('d-flex').addClass('d-block');
+            $('#status-space').removeClass('m-0');
+            $('p').css({
+                'text-align': 'left',
+                'margin-left': '1rem'
+            });
+        }
+    }
+
+    // Initial adjustment
+    adjustLayout();
+
+    // Adjust on window resize
+    $(window).resize(function() {
+        adjustLayout();
+    });
+});
