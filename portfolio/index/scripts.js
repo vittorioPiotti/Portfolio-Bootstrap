03c04bc49f56c1a5f345b520c8b4577bc2c33adb
vittorioPiotti <vittoriopiotti.vp@gmail.com>
2024-06-18 10:00:31 +0200

    Update scripts.js

--- portfolio/index/scripts.js
+++ portfolio/index/scripts.js
@@ -32,7 +32,18 @@ $(document).ready(function() {
         });
     });
 
-
+    
+    $(document).ready(function(){
+        $('a[href^="#"]').on('click', function(event) {
+            var target = $(this.getAttribute('href'));
+            if(target.length) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 700);
+            }
+        });
+    });
 
     $(document).ready(function() {
         // Funzione per aggiornare il padding degli elementi nav-elem
